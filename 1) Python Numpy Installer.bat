1838f07afb49db3605f8f5e12229eaefd6b23f3b
ItzBLAZE-01 <89532972+ItzBLAZE-01@users.noreply.github.com>
2023-11-20 18:36:23 +0530

    Add files via upload

---
+++ 1) Python Numpy Installer.bat
@@ -0,0 +1,6 @@
+echo off
+mode 55, 9
+title Installing Required Pips for the Crack
+echo Cracked By Kangaroo Team, Server Emulated by Ape Team, Fixed by @itzblaze_001 on discord
+python -m pip install --upgrade pip
+pip install numpy
---
+++ 2) Python PIPs Installer.bat
@@ -0,0 +1,5 @@
+echo off
+mode 55, 9
+title Installing Numpy for the PIPs
+echo Cracked By Kangaroo Team, Server Emulated by Ape Team, Fixed by @itzblaze_001 on discord
+pip install -r required_pips.txt
