aa0b2f7b87462cab07fa489c999cd93e7bab5df7
ItzBLAZE-01 <89532972+ItzBLAZE-01@users.noreply.github.com>
2024-01-03 15:43:04 +0530

    Update 2) Python PIPs Installer.bat

--- 2) Python PIPs Installer.bat
+++ 2) Python PIPs Installer.bat
@@ -1,5 +1,5 @@
 echo off
 mode 55, 9
-title Installing Numpy for the PIPs
+title Installing PIPs for the crack
 echo Cracked By Kangaroo Team, Server Emulated by Ape Team, Fixed by @itzblaze_001 on discord
 pip install -r required_pips.txt
