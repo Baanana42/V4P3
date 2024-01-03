8f73a507f81b61642e0a1d4f0e1335f49a72f7c1
ItzBLAZE-01 <89532972+ItzBLAZE-01@users.noreply.github.com>
2024-01-03 15:42:20 +0530

    Changed the title that appears when run.

--- 1) Python Numpy Installer.bat
+++ 1) Python Numpy Installer.bat
@@ -1,6 +1,6 @@
 echo off
 mode 55, 9
-title Installing Required Pips for the Crack
+title Installing Numpy Pip for the crack
 echo Cracked By Kangaroo Team, Server Emulated by Ape Team, Fixed by @itzblaze_001 on discord
 python -m pip install --upgrade pip
 pip install numpy
