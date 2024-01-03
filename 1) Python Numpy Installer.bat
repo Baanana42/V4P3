c113be9a4f9d863074c3f25f82e7cbbf2e9063a5
ItzBLAZE-01 <89532972+ItzBLAZE-01@users.noreply.github.com>
2024-01-03 15:43:35 +0530

    Update 1) Python Numpy Installer.bat

--- 1) Python Numpy Installer.bat
+++ 1) Python Numpy Installer.bat
@@ -1,6 +1,6 @@
 echo off
 mode 55, 9
-title Installing Numpy Pip for the crack
+title Installing Numpy Pip for the PIPs
 echo Cracked By Kangaroo Team, Server Emulated by Ape Team, Fixed by @itzblaze_001 on discord
 python -m pip install --upgrade pip
 pip install numpy
